fe9aef44296a52918f2aebeeb11e252dbd26705c
FroVolod <frol_off@meta.ua>
2022-07-07 17:34:55 +0300

    refactoring

--- src/commands/account/create_subaccount/mod.rs
+++ src/commands/account/create_subaccount/mod.rs
@@ -1,9 +1,3 @@
-use strum::{EnumDiscriminants, EnumIter, EnumMessage};
-
-mod autogenerate_new_keypair;
-mod use_manually_provided_seed_phrase;
-mod use_public_key;
-
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 pub struct SubAccount {
     ///What is the sub-account ID?
@@ -11,7 +5,7 @@ pub struct SubAccount {
     ///Enter the amount for the subaccount
     initial_balance: crate::common::NearBalance,
     #[interactive_clap(subcommand)]
-    pub access_key_mode: AccessKeyMode,
+    pub access_key_mode: super::add_key::AccessKeyMode,
 }
 
 impl SubAccount {
@@ -38,49 +32,10 @@ impl SubAccount {
             ],
         };
         self.access_key_mode
-            .process(prepopulated_unsigned_transaction)
+            .process(
+                prepopulated_unsigned_transaction,
+                near_primitives::account::AccessKeyPermission::FullAccess,
+            )
             .await
-    }
-}
-
-#[derive(Debug, Clone, EnumDiscriminants, interactive_clap::InteractiveClap)]
-#[strum_discriminants(derive(EnumMessage, EnumIter))]
-///Add a full access key for the sub-account
-pub enum AccessKeyMode {
-    #[strum_discriminants(strum(message = "Automatically generate a key pair"))]
-    ///Automatically generate a key pair
-    AutogenerateNewKeypair(self::autogenerate_new_keypair::GenerateKeypair),
-    #[strum_discriminants(strum(message = "Use the provided seed phrase manually"))]
-    ///Use the provided seed phrase manually
-    UseManuallyProvidedSeedPhrase(
-        self::use_manually_provided_seed_phrase::AddAccessWithSeedPhraseAction,
-    ),
-    #[strum_discriminants(strum(message = "Use the provided public key manually"))]
-    ///Use the provided public key manually
-    UseManuallyProvidedPublicKey(self::use_public_key::AddAccessKeyAction),
-}
-
-impl AccessKeyMode {
-    pub async fn process(
-        &self,
-        prepopulated_unsigned_transaction: near_primitives::transaction::Transaction,
-    ) -> crate::CliResult {
-        match self {
-            AccessKeyMode::UseManuallyProvidedPublicKey(add_access_key_action) => {
-                add_access_key_action
-                    .process(prepopulated_unsigned_transaction)
-                    .await
-            }
-            AccessKeyMode::AutogenerateNewKeypair(generate_keypair) => {
-                generate_keypair
-                    .process(prepopulated_unsigned_transaction)
-                    .await
-            }
-            AccessKeyMode::UseManuallyProvidedSeedPhrase(add_access_with_seed_phrase_action) => {
-                add_access_with_seed_phrase_action
-                    .process(prepopulated_unsigned_transaction)
-                    .await
-            }
-        }
     }
 }
