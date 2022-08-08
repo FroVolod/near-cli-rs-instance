9f81672f39786045089b6efc84082b55ca512bcb
FroVolod <frol_off@meta.ua>
2022-08-08 11:40:52 +0300

    config_prob

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,65 @@
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Config {
-    networks: std::collections::HashMap<String, NetworkConfig>,
+    pub networks: std::collections::HashMap<String, NetworkConfig>,
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct NetworkConfig {
-    url: url::Url,
+    pub url: url::Url,
     api_key: Option<String>,
 }
+
+// impl Default for Config {
+//     fn default() -> Self {
+//         let mut networks = std::collections::HashMap::new();
+//         networks.insert("mainnet", NetworkConfig {
+//             rpc_url: url::Url::new("https://archival-rpc.mainnet.near.org").unwrap(),
+//             // wallet_url
+//             // explorer_transaction_url
+//             api_key: None,
+//         });
+//         networks.insert("testnet", NetworkConfig {
+//             url: url::Url::new("https://archival-rpc.testnet.near.org").unwrap(),
+//             api_key: None,
+//         });
+//          networks.insert("localnet", NetworkConfig {
+//             url: url::Url::new("http://127.0.0.1:3030").unwrap(),
+//             api_key: None,
+//         });
+//         Self {
+//             networks
+//         }
+//     }
+// }
+
+// #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+// pub struct Config {
+//     credentials_home_dir: std::path::PathBuf, // <HOME>/.near-credentials + <network.network_name>
+//     networks: std::collections::HashMap<String, NetworkConfig>,
+// }
+
+// #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+// pub struct NetworkConfig {
+//     network_name: Option<String>, // mainnet
+//     url: url::Url,
+//     api_key: Option<String>,
+// }
+
+
+// r#"
+//         credentials_home_dir = "~/.near-credentials/" // + <chain_id>
+
+//         [networks.mainnet-qbit]
+//         chain_id = "mainnet"
+//         url = "https://mainnet.near.qbit.org.ua"
+
+//         [networks.mainnet-pagoda]
+//         chain_id = "mainnet"
+//         url = "https://rpc.mainnet.pagoda.co"
+ 
+//         [networks.testnet]
+//         url = "https://archival-rpc.testnet.near.org"
+
+//         [networks.localnet]
+//         url = "http://127.0.0.1:3030"
+//     "#,
--- src/network_for_transaction/mod.rs
+++ src/network_for_transaction/mod.rs
@@ -1,13 +1,36 @@
+use dialoguer::{theme::ColorfulTheme, Input, Select};
+
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(context = crate::GlobalContext)]
 pub struct NetworkForTransactionArgs {
     ///What is the name of the network
+    #[interactive_clap(skip_default_input_arg)]
     network_name: String,
     #[interactive_clap(subcommand)]
     transaction_signature_options: crate::transaction_signature_options::SignWith,
 }
 
 impl NetworkForTransactionArgs {
+    fn input_network_name(context: &crate::GlobalContext) -> color_eyre::eyre::Result<String> {
+        println!("---  context: {:#?}", &context.0.networks);
+        let variants = context.0.networks.keys().collect::<Vec<_>>();
+        // let submits = variants
+        //     .iter()
+        //     .map(|p| p.get_message().unwrap().to_owned())
+        //     .collect::<Vec<_>>();
+        let select_submit = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("What is the name of the network?")
+            .items(&variants)
+            .default(0)
+            .interact()
+            .unwrap();
+        let url = context.0.networks.get(variants[select_submit]).unwrap().url.clone();
+        Ok(variants[select_submit].to_string())
+        // Ok(Input::new()
+        //     .with_prompt("What is the name of the network?")
+        //     .interact_text()?)
+    }
+
     pub fn get_connection_config(
         &self,
         config: crate::config::Config,
